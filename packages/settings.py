1ea1ec11655c7991bf9656689846535a62834980
AmineRez <128901479+AmineRez@users.noreply.github.com>
2024-03-18 15:13:04 +0100

    Update settings.py

--- packages/settings.py
+++ packages/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gw!kexjz1qxk)u^tkep(7%vtg_+@&i!$-q-y_v0b1sjseqhb1&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
